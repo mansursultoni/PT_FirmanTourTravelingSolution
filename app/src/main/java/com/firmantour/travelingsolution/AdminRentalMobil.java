ac4e677ad69b03d7ac297d37a4511ef0b00f820c
Mansur Sultoni <sultoni.pyar@gmail.com>
2023-05-04 20:45:26 +0700

    Menyelesaikan Tampilan dan Koding - AdminRentalMobil - AdminDetailRentalMobil - TambahRentalMobil

--- app/src/main/java/com/firmantour/travelingsolution/AdminRentalMobil.java
+++ app/src/main/java/com/firmantour/travelingsolution/AdminRentalMobil.java
@@ -2,21 +2,47 @@ package com.firmantour.travelingsolution;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.PopupMenu;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.squareup.picasso.Picasso;
+
+import static com.firmantour.travelingsolution.R.drawable.ic_user;
 
 public class AdminRentalMobil extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
+
+    private FirebaseFirestore firebaseFirestore;
+    private FirestoreRecyclerAdapter adapter;
+    RecyclerView recyclerView;
+    ProgressBar progressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +50,17 @@ public class AdminRentalMobil extends AppCompatActivity implements PopupMenu.OnM
         setContentView(R.layout.activity_admin_rental_mobil);
 
 
+        Window window = this.getWindow();
+        window.setStatusBarColor(this.getResources().getColor(R.color.blue));
 
+
+        recyclerView = findViewById(R.id.recyclerView);
+        progressBar = findViewById(R.id.progressBar);
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+
+        getData();
 
         FloatingActionButton fab = findViewById(R.id.floatingActionButton);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -33,6 +69,82 @@ public class AdminRentalMobil extends AppCompatActivity implements PopupMenu.OnM
                 startActivity(new Intent(AdminRentalMobil.this, TambahRentalMobil.class));
             }
         });
+    }
+
+    private void getData() {
+        Query query = firebaseFirestore.collection("RentalMobil");
+        FirestoreRecyclerOptions<ClassProduk> response = new FirestoreRecyclerOptions.Builder<ClassProduk>()
+                .setQuery(query, ClassProduk.class).build();
+        adapter = new FirestoreRecyclerAdapter<ClassProduk, ProdukHolder>(response) {
+            @NonNull
+            @Override
+            public ProdukHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_produk, parent, false);
+                return new ProdukHolder(view);
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull ProdukHolder holder, int position, @NonNull final ClassProduk model) {
+                progressBar.setVisibility(View.GONE);
+                if (model.getFoto() != null) {
+                    Picasso.get().load(model.getFoto()).fit().into(holder.fotoProduk);
+                } else {
+                    Picasso.get().load(ic_user).fit().into(holder.fotoProduk);
+                }
+                holder.namaProduk.setText(model.getNama());
+                holder.hargaProduk.setText(model.getHarga());
+                holder.statusProduk.setText(model.getStatus());
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(AdminRentalMobil.this, AdminDetailRentalMobil.class);
+                        intent.putExtra("nomor", model.getNomor());
+                        startActivity(intent);
+//Snackbar.make(recyclerView, model.getNama()+", " +model.getTelepon(), Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onError(@NonNull FirebaseFirestoreException e) {
+                Log.e("Ditemukan Error: ", e.getMessage());
+            }
+        };
+        adapter.notifyDataSetChanged();
+        recyclerView.setAdapter(adapter);
+    }
+
+    public class ProdukHolder extends RecyclerView.ViewHolder {
+        ImageView fotoProduk;
+        TextView namaProduk, hargaProduk, statusProduk;
+        ConstraintLayout constraintLayout;
+
+        public ProdukHolder(@NonNull View itemView) {
+            super(itemView);
+            fotoProduk = itemView.findViewById(R.id.imageViewFoto);
+            namaProduk = itemView.findViewById(R.id.textViewNama);
+            hargaProduk = itemView.findViewById(R.id.textViewHarga);
+            statusProduk = itemView.findViewById(R.id.textStatus);
+//            constraintLayout = itemView.findViewById(R.id.constraintLayout);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
     }
 
     public void showPopup(View v){
---
+++ app/src/main/java/com/firmantour/travelingsolution/ClassProduk.java
@@ -0,0 +1,65 @@
+package com.firmantour.travelingsolution;
+
+public class ClassProduk {
+    private String nama, nomor, harga, deskripsi,status, foto;
+
+    public ClassProduk() {
+    }
+
+    public ClassProduk(String nama, String nomor, String harga, String deskripsi, String status, String foto) {
+        this.nama = nama;
+        this.nomor = nomor;
+        this.harga = harga;
+        this.deskripsi = deskripsi;
+        this.status = status;
+        this.foto = foto;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNomor() {
+        return nomor;
+    }
+
+    public void setNomor(String nomor) {
+        this.nomor = nomor;
+    }
+
+    public String getHarga() {
+        return harga;
+    }
+
+    public void setHarga(String harga) {
+        this.harga = harga;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+}
